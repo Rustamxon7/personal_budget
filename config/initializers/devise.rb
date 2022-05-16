524bb864c5af51f3f41057c5008e1aefb31e4c68
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-16 12:33:05 +0500

    Update expiring time to jwt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,7 +264,7 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   config.navigational_formats = ['*/*', :html]
-  
+
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
@@ -273,7 +273,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    jwt.expiration_time = 3.days.to_i
   end
 
   # The default HTTP method used to sign out a resource. Default is :delete.
