ebe7c0f6a53cd1455787bacb0a7e1cc93ae8c1d9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-19 12:46:57 +0500

    Validate user name and icon

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
+  validates :icon, presence: true
 end
