8a4aa603cbe3109dbab77f34f4705db22cd60b37
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-23 15:03:34 +0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module PersonalBudget
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
-    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.secret_key_base = ENV["DEVISE_JWT_SECRET_KEY"]
   end
 end
