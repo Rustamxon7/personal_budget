04436c8281fffba72a7ec5cb194ff4764f852f82
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-23 14:24:39 +0500

    dispatch request for all requests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -267,12 +267,6 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
-    ]
     jwt.expiration_time = 3.days.to_i
   end
 
