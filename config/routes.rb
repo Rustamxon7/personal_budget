991c92959d3b485fb24fed7761325557fbe51c92
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-30 17:54:57 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :people, only: [:index, :show, :create, :update, :destroy]
+      resources :categories, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
