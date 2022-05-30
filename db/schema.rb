227dd82e3a8d3303edef664195bddcec8de61f15
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-30 17:53:58 +0500

    Add Categories to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_120752) do
+ActiveRecord::Schema.define(version: 2022_05_25_085949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "money", null: false
+    t.string "icon", null: false
+    t.integer "persons_array", default: [], array: true
+    t.integer "person_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
@@ -39,5 +51,6 @@ ActiveRecord::Schema.define(version: 2022_05_23_120752) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "people", "users"
 end
