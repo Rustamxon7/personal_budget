af4a8727c62f7f2d8639e4f25a4379fd19680f04
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-31 18:51:26 +0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_085949) do
+ActiveRecord::Schema.define(version: 2022_05_30_193812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2022_05_25_085949) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "funds", force: :cascade do |t|
+    t.string "title"
+    t.integer "amount"
+    t.string "type_operation"
+    t.datetime "date"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_funds_on_category_id"
   end
 
   create_table "people", force: :cascade do |t|
@@ -52,5 +63,6 @@ ActiveRecord::Schema.define(version: 2022_05_25_085949) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "funds", "categories"
   add_foreign_key "people", "users"
 end
