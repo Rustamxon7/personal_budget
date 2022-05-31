3256135b564c64771d7ef733e19c0ee1b0dcd8d7
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-31 18:53:45 +0500

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :funds, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
   validates :icon, presence: true
