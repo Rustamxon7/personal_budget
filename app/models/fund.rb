3fd649e6b7528ee04a7258681fbebbf7104aa582
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-02 15:10:48 +0500

    Update fund.rb

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -2,4 +2,5 @@ class Fund < ApplicationRecord
   belongs_to :category
 
   validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :category_id }
+  validates :note, presence: true, length: { maximum: 255 }
 end
