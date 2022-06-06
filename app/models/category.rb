af7483e9d2505b1a4c525b930ac585e947bf974e
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:20:13 +0500

    Sum funds

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,17 @@
 class Category < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
+  belongs_to :person
   has_many :funds, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
   validates :icon, presence: true
+
+  def sum_funds
+    # sum all amounts of funds for each category
+    funds.sum(:amount)
+  end
+
+  def last_funds
+    funds.order(date: :desc).limit(10)
+  end
 end
