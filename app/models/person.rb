65cac86141b399f3220ef52347ba7a762bfa92fb
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:20:44 +0500

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,16 @@ class Person < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: { scope: :user_id }
   validates :icon, presence: true
+
+  has_many :categories, dependent: :destroy
+
+  after_save :update_categories
+
+  private
+
+  def update_categories
+    categories.each do |category|
+      category.update(person_id: id)
+    end
+  end
 end
