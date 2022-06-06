9f4227d681c26728014d8f60018e7c10282f7a40
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:18:19 +0500

    Make it for current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  has_many :categories, dependent: :destroy
+  has_many :funds, through: :categories
+  has_many :people, dependent: :destroy
 end
