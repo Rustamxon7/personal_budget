a2bbfd502213ef2a944c97703487e5bf17fb2c00
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:20:36 +0500

    Load last latest funds

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -3,4 +3,16 @@ class Fund < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }, uniqueness: { scope: :category_id }
   validates :note, presence: true, length: { maximum: 255 }
+
+  def last_funds
+    category.funds.order(created_at: :desc).limit(10)
+  end
+
+  def last_incomes
+    category.funds.where(type_declaration: 'incomes').order(date: :desc).limit(10)
+  end
+
+  def last_expenses
+    category.funds.where(type_declaration: 'expenses').order(date: :desc).limit(10)
+  end
 end
