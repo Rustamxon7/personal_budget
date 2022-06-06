72fab7660e0ac04be5fa7d3bc5dc97bd4eb92397
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-06 23:17:42 +0500

    Add type declaration

--- db/migrate/20220530193812_create_funds.rb
+++ db/migrate/20220530193812_create_funds.rb
@@ -4,6 +4,8 @@ class CreateFunds < ActiveRecord::Migration[6.1]
       t.string :title
       t.integer :amount
       t.string :type_operation
+      t.string :type_declaration
+      t.string :icon
       t.datetime :date
       t.text :note
       t.belongs_to :category, null: false, foreign_key: true
